2138480d934679bb164f8d4353c39804236f683b
Johannes Willkomm <johannes@johannes-willkomm.de>
2024-11-18 22:25:46 +0100

    Create version tag upon build

--- plugin.py
+++ plugin.py
@@ -1,6 +1,10 @@
 import subprocess
+import sys
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+sys.path += ['.']
+import msglen
 
 
 def patchfile(fname, pat, subst):
@@ -24,11 +28,14 @@ class SpecialBuildHook(BuildHookInterface):
 
     def initialize(self, *args):
         super().initialize(*args)
-        # rc = subprocess.run(['pwd'])
         rc = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+
         commitid = rc[0:6].decode('latin1')
         # print(commitid)
 
         patchfile('msglen/__init__.py',
                   '__commit__',
                   f'__commit__ = "{commitid}"\n')
+
+        rc = subprocess.run(['git', 'tag', '-f', f'{msglen.__version__}'])
+        assert rc.returncode == 0
