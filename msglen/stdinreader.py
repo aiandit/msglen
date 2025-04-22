8b18bfa90b83d1a8878665c9d31390331320452a
Johannes Willkomm <johannes@johannes-willkomm.de>
2025-04-22 18:12:21 +0200

    Prepare for new code

--- msglen/stdinreader.py
+++ msglen/stdinreader.py
@@ -77,9 +77,10 @@ async def connect_stdin_stdout():
     loop = asyncio.get_event_loop()
     reader = asyncio.StreamReader()
     protocol = asyncio.StreamReaderProtocol(reader)
-    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+    r_transport, r_protocol = await loop.connect_read_pipe(lambda: protocol, sys.stdin)
     w_transport, w_protocol = await loop.connect_write_pipe(asyncio.streams.FlowControlMixin, sys.stdout)
     writer = asyncio.StreamWriter(w_transport, w_protocol, reader, loop)
+    #return reader, writer, (r_transport, r_protocol, w_transport, w_protocol)
     return reader, writer
 
 
